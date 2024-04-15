7abf2d65cb634b4087ae7521a2125b596eb94d6b
Maria Vitória Amorim Farias Barbosa <vitamorim4002@gmail.com>
2024-04-15 20:47:50 -0300

    API test automation v-2

--- cypress/e2e/element-create.cy.js
+++ cypress/e2e/element-create.cy.js
@@ -1,7 +1,11 @@
 ///<reference types="cypress" />
 
 describe('Request and create a new element', () => {
+
+// -----------------POSTS----------------- 
+
     it('Create a new element in posts',()=>{
+
         cy.request({
             method : 'POST',
             url : '/posts',
@@ -19,25 +23,124 @@ describe('Request and create a new element', () => {
             expect(response.body).to.have.property('title', 'New element')
             expect(response.body).to.have.property('body', 'I am the body of the new element and I am here to stay')
         })
+
     })
+
+// -----------------COMMENTS-----------------
 
     it('Create a new element in comments', () => {
 
+        cy.request({
+            method : 'POST',
+            url : '/comments',
+            body: {
+                postId: 1,
+                name: 'complement to the author',
+                email: 'test@hotmail.com',
+                body: 'nice post dude ! hope to see more content about this topic !'
+            }
+        })
+        .then((response) =>{
+            cy.log(response)
+            expect(response.status).to.equal(201)
+            expect(response.body.postId).to.equal(1)
+            expect(response.body.name).to.exist
+            expect(response.body.email).to.equal('test@hotmail.com')
+            expect(response.body.body).to.exist
+        })
+
     });
+
+// -----------------ALBUMS-----------------
 
     it('Create a new element in albums', () => {
 
+        cy.request({
+            method : 'POST',
+            url : '/albums',
+            body : {
+                userId : 10,
+                id : 101,
+                title : 'THE goat album'
+            }
+        })
+        .then((response) =>{
+            cy.log(response)
+            expect(response.status).to.equal(201)
+            expect(response.body.id).to.equal(101)
+            expect(response.body.userId).to.equal(10)
+            expect(response.body.title).to.exist
+        })
+
     });
+
+// -----------------PHOTOS-----------------
 
     it('Create a new element in photos', () => {
 
+        cy.request({
+            method : 'POST',
+            url : '/photos',
+            body : {
+                albumId : 3,
+                title : 'photo of my happy family',
+                url : 'https://via.placeholder.com/600/92c952',
+                thumbnailUrl : 'https://via.placeholder.com/150/92c952'
+            }
+        })
+        .then((response) =>{
+            cy.log(response)
+            expect(response.status).to.equal(201)
+            expect(response.body.title).to.exist
+            expect(response.body.url).to.exist
+            expect(response.body.thumbnailUrl).to.exist
+        })
+
     });
+
+// -----------------TODOS-----------------
 
     it('Create a new element in todos', () => {
 
+        cy.request({
+            method : 'POST',
+            url : '/todos',
+            body : {
+                userId : 9,
+                title : 'call to cousin for his birthday',
+                completed : false
+            }
+        })
+        .then((response) =>{
+            cy.log(response)
+            expect(response.status).to.equal(201)
+            expect(response.body.userId).to.equal(9)
+            expect(response.body.title).to.exist
+            expect(response.body.completed).to.equal(false)
+        })
+
     });
+
+// -----------------USERS-----------------
 
     it('Create a new element in users', () => {
 
+        cy.request({
+            method : 'POST',
+            url : '/users',
+            body : {
+                name : 'John',
+                username : 'johnny',
+                email : 'johnnyboy@shelby.com',
+            }
+        })
+        .then((response) =>{
+            cy.log(response)
+            expect(response.status).to.equal(201)
+            expect(response.body.name).to.equal('John')
+            expect(response.body.username).to.equal('johnny')
+            expect(response.body.email).to.exist
+        })
+        
     });
 })
--- cypress/e2e/element-delete.cy.js
+++ cypress/e2e/element-delete.cy.js
@@ -2,34 +2,93 @@
 
 describe('Request and delete an element',()=>{
 
+// -----------------POSTS----------------- 
+
     it('Delete a specific element from posts', () => {
+
         cy.request({
             method : 'DELETE',
-            url : '/posts/28'
+            url : '/posts/2'
         })
         .then((response) => {
             cy.log(response)
             expect(response.status).to.equal(200)
         })
+
     });
+
+// -----------------COMMENTS-----------------
 
     it('Delete a specific element from comments', () => {
 
+        cy.request({
+            method : 'DELETE',
+            url : '/comments/5'
+        })
+        .then((response) => {
+            cy.log(response)
+            expect(response.status).to.equal(200)
+        })
+
     });
+
+// -----------------ALBUMS--------------------
 
     it('Delete a specific element from albums', () => {
 
+        cy.request({
+            method : 'DELETE',
+            url : '/albums/48'
+        })
+        .then((response) => {
+            cy.log(response)
+            expect(response.status).to.equal(200)
+        })
+
     });
+
+// -----------------PHOTOS--------------------
 
     it('Delete a specific element from photos', () => {
 
+        cy.request({
+            method : 'DELETE',
+            url : '/photos/4537'
+        })
+        .then((response) => {
+            cy.log(response)
+            expect(response.status).to.equal(200)
+        })
+
     });
+
+// -----------------TODOS--------------------
 
     it('Delete a specific element from todos', () => {
 
+        cy.request({
+            method : 'DELETE',
+            url : '/todos/31'
+        })
+        .then((response) => {
+            cy.log(response)
+            expect(response.status).to.equal(200)
+        })
+
     });
 
+// -----------------USERS--------------------
+
     it('Delete a specific element from users', () => {
+
+        cy.request({
+            method : 'DELETE',
+            url : '/users/6'
+        })
+        .then((response) => {
+            cy.log(response)
+            expect(response.status).to.equal(200)
+        })
 
     });
     
